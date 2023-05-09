17f35e8beffcaf7ce0688b2c87243fb4c9c25148
Tom JD1 Xue <xuejd1@lenovo.com>
2023-05-10 00:37:58 +0800

    workable

---
+++ MyPopupIssue/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyPopupIssue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyUserControl userControl = new MyUserControl(MyGrid);
+            MyGrid.Children.Add(userControl);
+            MyUserControl.ETestButtonMouseClick += MyUserControl_ETestButtonMouseClick;
+        }
+
+        private void MyUserControl_ETestButtonMouseClick(UIElement target)
+        {
+            Application.Current.Dispatcher.InvokeAsync(new Action(() =>
+            {
+                Popup popup = new Popup();
+                popup.AllowsTransparency = true;
+                popup.PlacementTarget = target;
+                popup.HorizontalOffset = 10;
+                popup.VerticalOffset = 41;
+                popup.Placement = PlacementMode.Left;
+                popup.StaysOpen = false;
+
+                TextBlock myTextBlock = new TextBlock();
+                myTextBlock.HorizontalAlignment = HorizontalAlignment.Center;
+                myTextBlock.VerticalAlignment = VerticalAlignment.Center;
+                myTextBlock.Width = 166;
+                myTextBlock.Height = 170;
+                myTextBlock.Text = "Just a test.";
+                myTextBlock.FontSize = 22;
+                myTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+                myTextBlock.Background = new SolidColorBrush(Colors.Yellow);
+                popup.Child = myTextBlock;
+                popup.IsOpen = true;
+            }));
+        }
+    }
+}
---
+++ MyPopupIssue/MyUserControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyPopupIssue
+{
+    /// <summary>
+    /// Interaction logic for MyUserControl.xaml
+    /// </summary>
+    public partial class MyUserControl : UserControl
+    {
+        UIElement Parent = null;
+        public delegate void TestButtonMouseClick(UIElement target);
+        public static event TestButtonMouseClick ETestButtonMouseClick;
+
+        public MyUserControl(UIElement parent)
+        {
+            InitializeComponent();
+            this.Parent = parent;
+        }
+
+        private void TestButton_Click(object sender, RoutedEventArgs e)
+        {
+            ETestButtonMouseClick?.Invoke(Parent);
+        }
+    }
+}
